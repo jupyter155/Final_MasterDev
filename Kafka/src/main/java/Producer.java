182a5fca9dcc41bf0ca418e5393306281588f1db
jupyter155 <nguyenxuanminh155@gmail.com>
2022-08-18 10:18:47 +0700

    first commit

---
+++ Kafka/src/main/java/Producer.java
@@ -0,0 +1,42 @@
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Properties;
+import java.util.Scanner;
+import static org.apache.kafka.common.requests.DeleteAclsResponse.log;
+
+import java.util.concurrent.TimeUnit;
+public class Producer {
+    static String PATH = "/home/minh/preprocess_data.csv";
+    public static void main(String[] args) throws FileNotFoundException, InterruptedException {
+
+        Properties props = new Properties();
+        props.put(ProducerConfig.CLIENT_ID_CONFIG,"my-producer-threading-demo");
+        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG,"172.17.80.23:9092"); // 172.17.80.20:9092
+        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG,StringSerializer.class.getName());
+
+        KafkaProducer<String,String> producer = new KafkaProducer<String, String>(props);
+
+        File file = new File(PATH);
+        String topicName = "minhnx12";
+        try(Scanner scanner = new Scanner(file)){
+            while(scanner.hasNextLine()){
+                String line = scanner.nextLine();
+                log.info(line);
+                producer.send(new ProducerRecord<>(topicName,null,line));
+                TimeUnit.SECONDS.sleep(1);
+                producer.flush();
+
+            }
+            log.info("Finished Sending");
+        }
+        producer.close();
+    }
+
+
+}
---
+++ Kafka/src/main/java/SparkStreaming.java
@@ -0,0 +1,54 @@
+
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
+import org.apache.spark.sql.SparkSession;
+import org.apache.spark.sql.Row;
+import org.apache.spark.sql.Dataset;
+
+import static org.apache.spark.sql.functions.col;
+import static org.apache.spark.sql.functions.split;
+
+public class SparkStreaming {
+    public static void main(String[] args) throws Exception {
+
+        Logger.getLogger("org").setLevel(Level.OFF);
+
+        // Define a Spark Session
+        SparkSession spark = SparkSession
+                .builder()
+                .appName("Spark Streaming")
+//                .master("local")
+//                .config("spark.driver.bindAddress", "127.0.0.1")
+                .getOrCreate();
+
+        Dataset<Row> ds = spark
+                .readStream()
+                .format("kafka")
+                .option("kafka.bootstrap.servers", "172.17.80.23:9092")
+                .option("subscribe", "minhnx12")
+                .option("startingOffsets", "earliest")
+                .load();
+
+        Dataset<Row> DF =
+                ds.selectExpr("CAST(value as string)")
+                        .select(split(col("value"),",").getItem(0).as("title"),
+                                split(col("value"),",").getItem(1).as("year"),
+                                split(col("value"),",").getItem(2).as("rating"),
+                                split(col("value"),",").getItem(3).as("runtime"),
+                                split(col("value"),",").getItem(4).as("kind"),
+                                split(col("value"),",").getItem(5).as("color_info"),
+                                split(col("value"),",").getItem(12).as("votes"),
+                                split(col("value"),",").getItem(13).as("country"),
+                                split(col("value"),",").getItem(14).as("day"),
+                                split(col("value"),",").getItem(15).as("month")
+                        );
+
+        DF.writeStream()
+                .outputMode("append")
+                .format("parquet")
+                .option("checkpointLocation", "/user/minhnx12/final/1") ///home/minh/Final_MasterDev/minhnx12/final/1
+                .option("path", "/user/minhnx12/final/2") ///home/minh/Final_MasterDev/minhnx12/final/2
+                .start().awaitTermination();
+    }
+}
+
